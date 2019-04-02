07fc4c0f538de6b5acf7b9ba45ac3cb3fc54c6d3
Kevin <kevin.bourdouleix@gmail.com>
2019-04-02 12:08:06 +0200

    Test de la limite à 99%

---
+++ stryker.conf.js
@@ -0,0 +1,10 @@
+module.exports = function(config) {
+  config.set({
+    mutator: "javascript",
+    packageManager: "npm",
+    reporters: ["clear-text", "progress", "dashboard"],
+    testRunner: "jest",
+    transpilers: [],
+    coverageAnalysis: "off"
+  });
+};
